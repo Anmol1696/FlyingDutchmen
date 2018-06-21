81b8329f978ffc12b8199dd79878de4a62cbf818
Anmol1696 <anmol1696@gmail.com>
2018-06-21 19:55:19 +0530

    Fomula for motor calculation

--- quadcopter/propulsion/motor.py
+++ quadcopter/propulsion/motor.py
@@ -12,3 +12,8 @@ def motor_induced_velocity(thrust, density_of_air, diameter_of_propeller):
     area_of_rotor_disk = math.pi*diameter_of_propeller**2/4.0
     return math.sqrt(thrust/(2.0*density_of_air*area_of_rotor_disk))
 
+def get_thrust_per_motor(total_mass, velocity, drag_coff, area_of_frame, density_of_air, g=9.8):
+    total_drag = 0.5*density_of_air*velocity**2*area_of_frame*drag_coff
+    thrust_per_motor = (total_mass*g + total_drag)/4.0
+
+    return thrust_per_motor
