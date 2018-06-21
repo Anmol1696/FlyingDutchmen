cb36ced5fe9fdd039de4e4682ea3e3a4be7072bb
Anmol1696 <anmol1696@gmail.com>
2018-06-21 19:21:43 +0530

    Cost function

---
+++ costfunction.py
@@ -0,0 +1,37 @@
+"""
+    This file contains cost function calculation
+"""
+
+import math
+
+def get_number_of_rounds(range_diameter, height_of_operration, field_angle):
+    return math.ceil(range_diameter/(2.0*height_of_operration*math.tan(field_angle*math.pi/(2.0*180.0))))
+
+def exploration_phase(num_of_rounds, height_of_operration, field_angle, velocity):
+    """
+        Returns the cost function value for the exploration phase
+        This minimizes the time taken for exploration
+        field_angle is in degrees
+    """
+    final_result = 0
+
+    for i in range(num_of_rounds):
+        temp_num = (2*math.pi*height_of_operration*math.tan(field_angle*math.pi/360.0)*(2*i - 1))
+        final_result += temp_num/(velocity*1.0)
+
+    return final_result
+
+if __name__ == "__main__":
+    """
+        Testing above functions
+    """
+    range_diameter = 1000
+    height_of_operration = 20
+    field_angle = 120
+    velocity = 10
+
+    number_of_rounds = int(get_number_of_rounds(range_diameter, height_of_operration, field_angle))
+    result = exploration_phase(number_of_rounds, height_of_operration, field_angle, velocity)
+
+    print 'Number of rounds ->', number_of_rounds
+    print 'Cost function value ->', (result)/(60*60.0)
